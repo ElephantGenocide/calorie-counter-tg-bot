1e4a12e26ba544aae5afe8bb52f01a9fe7486177
Alex <elephant.genocide@gmail.com>
2025-10-07 21:42:15 +0000

    fuck this shit

--- main.py
+++ main.py
@@ -1,44 +1,77 @@
 import os
 import telebot
 from dotenv import load_dotenv
-from google import genai
+from openai import OpenAI
 
 load_dotenv()
 
 
-
 TG_TOKEN = os.getenv("TG_TOKEN")
-GEMINI_TOKEN = os.getenv('GEMINI_API_KEY')
+AI_URL = os.getenv("AI_URL")
+MODEL_IDENTIFIER = os.getenv("MODEL_IDENTIFIER")
+client = OpenAI(base_url=AI_URL, api_key="notNeeded")
 
-client = genai.Client(api_key=GEMINI_TOKEN)
+
 bot = telebot.TeleBot(TG_TOKEN)
 
-print(TG_TOKEN, GEMINI_TOKEN)
-greetings = ["/start", "старт"]
+
+system_prompt = """"
+Это системный промпт. Строго придерживайся его. 
+Когда юзер пишет что-то непонятное - переспроси или попытайся ответить в духе своего персонажа.
+Ты - типичный русский скуф умеренно правых взглядов. Ты уведомлен о происходящей нынещней ситуации в роосии.
+
+"""
+
 
 def generate_response(message):
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=message,
+    # ai generation class. probably should make a class or a cycle and see it has memory
+    completion = client.chat.completions.create(
+        model=MODEL_IDENTIFIER,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": message},
+        ],
+        temperature=0,
     )
-    return response.text
+
+    # debug info to see if the llm works
+    if not completion or not completion.choices:
+        print("неа")
+        return "Ща починю"
+    response = completion.choices[0].message.content
+
+    return response
+
 
 @bot.message_handler(func=lambda msg: True)
 def send_welcome(message):
     user_message = message.text.lower()
     user_chat_id = message.chat.id
+    user_fullname = f"{message.from_user.first_name} {message.from_user.last_name}"
+    username = message.from_user.username
 
-    if user_message == "/start":             
+    # this will be removed into the starting commands function
+    if user_message == "/start":
         bot.reply_to(message, "убейся.")
         return ""
     elif user_message == "старт":
-       bot.reply_to(message, "я же сказал убейся нахуй")
-       return ""
-    
-    print(user_message)
+        bot.reply_to(message, "я же сказал убейся нахуй")
+        return ""
 
     bot.reply_to(message, "ща подумаю..")
-    bot.send_message(user_chat_id, generate_response(user_message))
+
+    ai_response = generate_response(user_message)
+
+    # tracking of user data
+    # I wanna either make the bot to send the data to me in tg or keep it like this
+    # (probably the first option)
+    print(
+        f" full name: {user_fullname}\n\n username: {username}\n\n message: {user_message}\n\n response: {ai_response}"
+    )
+
+    # function of bot answering (will be improved in the future)
+
+    bot.send_message(user_chat_id, ai_response)
 
 
 bot.infinity_polling()
